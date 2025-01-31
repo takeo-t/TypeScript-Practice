25d55afa4bc9434c24cc3de3caac921fd90895de
takeo-t <takeo-t@118satellite.com>
2025-01-31 23:10:16 +0900

    TSファイルをコンパイルすると型情報がなくなることを確認

---
+++ src/function.js
@@ -0,0 +1,4 @@
+var func = function (args1, args2) {
+    return args1 * args2;
+};
+console.log(func(1, 2));
---
+++ src/function.ts
@@ -0,0 +1,5 @@
+const func = (args1: number, args2: number) => {
+    return args1 * args2;
+}
+
+console.log(func(1, 2));
